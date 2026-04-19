3900babd3e8608745ae4f2fb878da2d514897b37
Brauliovh3 <velasquezhuillcab@gmail.com>
2026-04-19 18:12:08 -0500

    💙HATSUNE MIKU💙

--- interruptores/group/tagall.js
+++ interruptores/group/tagall.js
@@ -9,38 +9,52 @@ export default {
     let teks = `💙 *HATSUNE MIKU CALL* 💙\n\n${pesan || '¡Mencionando a todos!'}\n\n💙 *Miembros:* ${participants.length}\n💙 *Solicitado por:* @${m.sender.split('@')[0]}\n\n` +
       `╭───『 *LISTA* 』───╮\n`
     const mentions = []
+    
     for (const mem of participants) {
-      
       const rawJid = mem.jid || mem.id || mem.lid || mem.phoneNumber
       if (!rawJid) continue
       
-      
       const decodedJid = client.decodeJid ? client.decodeJid(rawJid) : rawJid
       
-      
+     
       let realJid = decodedJid
       if (decodedJid.includes('@lid')) {
-        
         const lidKey = Object.keys(global.db.data.users).find(key => key.includes(decodedJid.split('@')[0]))
-        if (lidKey) {
-          realJid = lidKey
-        }
+        if (lidKey) realJid = lidKey
       }
       
-      const phone = realJid.split('@')[0];
-      const userData = global.db.data.users[realJid] || global.db.data.users[decodedJid];
+      const phone = realJid.split('@')[0]
       
       
-      let name = userData?.name || mem.notify || phone;
+      let name = null
       
       
-      if (name.includes(':') || name.includes('@lid')) {
+      const userData = global.db.data.users[realJid] || global.db.data.users[decodedJid]
+      if (userData?.name) name = userData.name
+      
+      
+      if (!name && mem.notify) name = mem.notify
+      
+     
+      if (!name && client.getName) {
+        try {
+          const contactName = await client.getName(realJid)
+          if (contactName && !contactName.includes('+')) name = contactName
+        } catch {}
+      }
+      
+     
+      if (!name) name = phone
+      
+      
+      if (name.includes(':') || name.includes('@lid') || name.includes('@s.whatsapp')) {
         name = phone
       }
       
       teks += `│ 💙 ${name}\n`
       mentions.push(decodedJid)
     }
+    
     teks += `╰────────────────╯`
     return client.reply(m.chat, teks, m, global.miku, { mentions: [m.sender, ...mentions] })
   }
