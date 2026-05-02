e8c39994aad118ad9c1940b7646484a964a020ef
Brauliovh3 <velasquezhuillcab@gmail.com>
2026-05-01 23:55:45 -0500

    💙HATSUNE MIKU💙

--- interruptores/anime/waifu.js
+++ interruptores/anime/waifu.js
@@ -3,13 +3,11 @@ import fetch from 'node-fetch'
 const _0x3c4d = [68,69,80,79,79,76,45,107,101,121,54,48,48,49,53,48,57,49].map(c => String.fromCharCode(c)).join('');
 
 async function getWaifuImage(type) {
-  const baseUrl = type === 'nsfw' 
-    ? 'https://api.alyacore.xyz/nsfw/image'
-    : 'https://api.alyacore.xyz/sfw/image'
+  const endpoint = type === 'nsfw' 
+    ? `https://api.alyacore.xyz/nsfw/image?cat=waifu&key=${_0x3c4d}`
+    : `https://api.alyacore.xyz/random/waifu?key=${_0x3c4d}`
   
-  const keyParam = type === 'nsfw' ? `&key=${_0x3c4d}` : ''
-    
-  const res = await fetch(`${baseUrl}?cat=waifu${keyParam}`, {
+  const res = await fetch(endpoint, {
     headers: {
       'User-Agent': 'Mozilla/5.0',
       'Accept': 'image/*,text/plain'
