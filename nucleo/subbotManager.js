1c42116d2c94ce69fffb458c214916dce1dfa576
Brauliovh3 <velasquezhuillcab@gmail.com>
2026-05-03 10:24:54 -0600

    💙HATSUNE MIKU💙

--- nucleo/subbotManager.js
+++ nucleo/subbotManager.js
@@ -105,6 +105,7 @@ const upsertConn = (sock, sessionId) => {
     }
   }
   global.conns.push(sock)
+  console.log(`[upsertConn] Registered ${userId} in global.conns, total: ${global.conns.length}`)
 }
 
 
