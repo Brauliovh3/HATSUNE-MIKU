edd6114b1e78df9f985c501e67852fcd5e6704df
Brauliovh3 <velasquezhuillcab@gmail.com>
2026-05-10 00:05:17 -0600

    💙HATSUNE MIKU💙

--- interruptores/nsfw/hgames.js
+++ interruptores/nsfw/hgames.js
@@ -1,31 +1,33 @@
 import { prepareWAMessageMedia, generateWAMessageFromContent, getDevice } from '@whiskeysockets/baileys'
 
 const games = [
-  { name: 'Teaching Feeling v3.0', size: '519 MB', file: 'Teaching-Feeling.apk' },
-  { name: 'Lonely Girl v1.0', size: '39.5 MB', file: 'Lonely.Girl.apk' },
-  { name: 'FHB v1.0', size: '40.7 MB', file: 'FHBQuickieHalloween.Mavis.apk' },
+  { name: 'Teaching Feeling v3.0', size: '531 MB', file: 'Teaching-Feeling.apk' },
+  { name: 'Lonely Girl v1.0', size: '39 MB', file: 'Lonely.Girl.apk' },
+  { name: 'FHB v1.0', size: '40 MB', file: 'FHBQuickieHalloween.Mavis.apk' },
   { name: 'Kaguya Player v2.0', size: '49 MB', file: 'KAGUYA_PLAYER.apk' },
-  { name: 'Coco-nut Shake v1.5', size: '42.2 MB', file: 'Coco-nut_shake.apk' },
-  { name: 'Tatsumaki-TH v1.0', size: '30.3 MB', file: 'Tatsumaki-TH.apk' },
-  { name: 'Nicole v1 v1.17', size: '48.4 MB', file: 'Nicole.v1.17.apk' },
+  { name: 'Coco-nut Shake v1.5', size: '42 MB', file: 'Coco-nut_shake.apk' },
+  { name: 'Tatsumaki-TH v1.0', size: '30 MB', file: 'Tatsumaki-TH.apk' },
+  { name: 'Nicole v1 v1.17', size: '48 MB', file: 'Nicole.v1.17.apk' },
   { name: 'Fapwall v1.0', size: '13.5 MB', file: 'Fapwall.apk' },
   { name: 'Fuckerwatch v1.0', size: '63.2 MB', file: 'FUCKERWATCH.apk' },
-  { name: 'Survive v1.0', size: '46.1 MB', file: 'survive.apk' },
+  { name: 'Survive v1.0', size: '46 MB', file: 'survive.apk' },
   { name: 'Together Again', size: '298 MB', file: 'Together_Again.apk' },
   { name: 'The-Queen-Of-Martial', size: '157 MB', file: 'The-Queen-Of-Martial.apk' },
-  { name: 'Lovely Piston Trap', size: '93.5 MB', file: 'LovelyCraftPistonTrap.apk' },
+  { name: 'Lovely Piston Trap', size: '93 MB', file: 'LovelyCraftPistonTrap.apk' },
   { name: 'Intimate Brothel', size: '151 MB', file: 'Intimate-Brothel.apk' },
   { name: 'My College', size: '180 MB', file: 'My_College.apk' },
   { name: 'Pocket Touch Simulation', size: '395 MB', file: 'Pocket_Touch_Simulation.apk' },
   { name: 'Shopkeepers Wife NTR', size: '221 MB', file: 'Shopkeepers.Wife.NTR.apk' },
-  { name: 'Sister Fight', size: '56.4 MB', file: 'Sister_Fight.apk' },
+  { name: 'Sister Fight', size: '56 MB', file: 'Sister_Fight.apk' },
   { name: 'Pocket Sweeties 2', size: '340 MB', file: 'PocketSweeties2.apk' },
   { name: 'Horny Union', size: '249 MB', file: 'Horny.Union.apk' },
-  { name: 'Sweet Deviance', size: '394 MB', file: 'SweetDeviance.apk' },
-  { name: 'Happy Summer', size: '408 MB', file: 'HS.apk' },
-  { name: 'My Daughter Forever', size: '405 MB', file: 'MyDaughterForever.apk' },
+  { name: 'Sweet Deviance', size: '393 MB', file: 'SweetDeviance.apk' },
+  { name: 'Happy Summer', size: '407 MB', file: 'HS.apk' },
+  { name: 'My Daughter Forever', size: '404 MB', file: 'MyDaughterForever.apk' },
   { name: 'My Best Deal', size: '519 MB', file: 'MY.BEST.DEAL.apk' },
-  { name: 'Nemurimouto', size: '240 MB', file: 'NEMURIMOUTO.apk' }
+  { name: 'Nemurimouto', size: '240 MB', file: 'NEMURIMOUTO.apk' },
+  { name: 'Bosque Minielfas', size: '430 MB', file: 'Bosque_minielfas.apk' },
+  { name: 'Kizuna player', size: '37 MB', file: 'KIZUNA-PLAYER-v2.1.0-JuegosH.com.apk' }
 ]
 
 const COVER_URL = 'https://cdn.somoskudasai.com/image/b41e537b8184463d78b6b98b3e382938/1920x1080/portada_hatsune-miku-38.jpg'
